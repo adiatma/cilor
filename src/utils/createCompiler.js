a02b03dfcacd9f1438e0010524977532471c1b89
adiatma <adiatma.kamaarudin@ralali.com>
2019-10-11 22:53:44 +0700

    v1.1.8

--- src/utils/createCompiler.js
+++ src/utils/createCompiler.js
@@ -5,37 +5,37 @@ const webpack = require('webpack')
  * @param {Object} config
  */
 function createCompiler(config) {
-  // /**
-  //  * @see https://webpack.js.org/api/node/#webpack
-  //  */
-  // const compiler = webpack(config)
+  /**
+   * @see https://webpack.js.org/api/node/#webpack
+   */
+  const compiler = webpack(config)
 
-  // compiler.run((err, stats) => {
-  //   /**
-  //    * @see https://webpack.js.org/api/node/#statshaserrors
-  //    */
-  //   if (err || stats.hasErrors()) {
-  //     process.stdout.write(
-  //       stats.toString({chunks: false, colors: true}) + '\n',
-  //     )
-  //   }
+  compiler.run((err, stats) => {
+    /**
+     * @see https://webpack.js.org/api/node/#statshaserrors
+     */
+    if (err || stats.hasErrors()) {
+      process.stdout.write(
+        stats.toString({chunks: false, colors: true}) + '\n',
+      )
+    }
 
-  //   /**
-  //    * @see https://webpack.js.org/api/node/#statshaswarnings
-  //    */
-  //   if (stats.hasWarnings()) {
-  //     process.stdout.write(
-  //       stats.toString({chunks: false, colors: true}) + '\n',
-  //     )
-  //   }
+    /**
+     * @see https://webpack.js.org/api/node/#statshaswarnings
+     */
+    if (stats.hasWarnings()) {
+      process.stdout.write(
+        stats.toString({chunks: false, colors: true}) + '\n',
+      )
+    }
 
-  //   /**
-  //    * @see https://webpack.js.org/api/node/#statstostringoptions
-  //    */
-  //   process.stdout.write(
-  //     stats.toString({chunks: false, colors: true}) + '\n',
-  //   )
-  // })
+    /**
+     * @see https://webpack.js.org/api/node/#statstostringoptions
+     */
+    process.stdout.write(
+      stats.toString({chunks: false, colors: true}) + '\n',
+    )
+  })
 }
 
 module.exports = createCompiler
